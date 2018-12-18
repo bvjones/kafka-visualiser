9b42c90e282fc1cdb26601a3fafe42bbceb96e2a
Rick Clark <rkingsleyclark@gmail.com>
2018-12-18 10:39:47 +0000

    Increment event counts in app state

--- app/src/components/App/index.js
+++ app/src/components/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import openSocket from 'socket.io-client';
+import get from 'lodash.get';
 import './index.css';
 
 class App extends Component {
@@ -8,15 +9,45 @@ class App extends Component {
   constructor() {
     super();
     this.socket = null;
+
+    this.state = {
+      events: {},
+    }
+
+    this.incrementEventCount = this.incrementEventCount.bind(this);
   }
 
+
+  incrementEventCount(eventName) {
+    const event = get(this.state, `events[${eventName}]`);
+
+      return this.setState({
+        events: {
+          ...this.state.events,
+          [eventName]: {
+            count: event ? event.count + 1 : 1,
+          }
+        }
+      })
+
+  }
+  
   componentWillMount() {
-     this.socket = openSocket('http://localhost:3001');
+      this.socket = openSocket('http://localhost:3001');
 
-     this.socket.on('kafkaEvent', function (data) {
-      console.log(data);
-    });
+      this.socket.on('kafkaEvent', function (data) {
+      const event = JSON.parse(data.value);
+      const {eventName} = event.metadata;
+
+      console.log(eventName);
+      this.incrementEventCount(eventName);
+    }.bind(this));
   }
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
 
   render() {
     return (
