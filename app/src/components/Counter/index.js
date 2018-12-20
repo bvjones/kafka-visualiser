a1094fab0a814a5b6719a230cda90404520e8476
Rick Clark <rkingsleyclark@gmail.com>
2018-12-20 14:01:59 +0000

    Format counter

--- app/src/components/Counter/index.js
+++ app/src/components/Counter/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import PropTypes from 'prop-types';
 import styles from './index.module.css';
 import ColorHash from "color-hash";
+import numeral from 'numeral';
 
 const colorHash = new ColorHash();
 
 export default function Counter({ name, count }) {
+  const displayCount = count < 1000 ? count : numeral(count).format('0.0a');
+
   return (
     <div className={styles.counterContainer}>
-      <span className={styles.count} style={{color: colorHash.hex(name)}}>{count}</span>
+      <span className={styles.count} style={{color: colorHash.hex(name)}}>{displayCount}</span>
       <span className={styles.name}>{name}</span>
     </div>
   );
