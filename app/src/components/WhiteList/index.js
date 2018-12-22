953c705b6fe559666b5ccd6883ce8061d2d61621
Rick Clark <rkingsleyclark@gmail.com>
2018-12-22 21:52:39 +0000

    WIP: Style checkboxes

--- app/src/components/WhiteList/index.js
+++ app/src/components/WhiteList/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './index.module.css';
 
 export default function WhiteList({ events, updateEventWhitelist }) {
 
   const eventSelects = Object.entries(events).map(({ 0: name, 1: value }) => {
     return (
-      <div key={name}>
-        <label htmlFor={name}>{name}</label>
+      <div key={name} className={styles.checkboxContainer}>
         <input
+          className={styles.checkbox}
           type="checkbox"
           name={name}
           id={name}
           checked={value.whitelisted}
           onChange={updateEventWhitelist}
         />
+        <label htmlFor={name} className={styles.checkboxLabel}>{name}</label>
       </div>
     );
   });
