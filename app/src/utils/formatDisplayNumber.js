c061bd953efb0305493b32a81e4adfc54bf1c932
Rick Clark <rkingsleyclark@gmail.com>
2018-12-31 12:12:44 +0000

    Update number formats

--- app/src/utils/formatDisplayNumber.js
+++ app/src/utils/formatDisplayNumber.js
@@ -1,6 +1,6 @@
 import numeral from 'numeral';
 
 export default function formatDisplayNumber(number) {
-  const displayNumber = number < 1000 ? number : numeral(number).format('0.0a');
+  const displayNumber = number < 1000 ? numeral(number).format('0.[0]') : numeral(number).format('0.0a');
   return displayNumber;
 }
